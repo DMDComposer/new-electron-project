3fd2f9fd93ce4fda27bb458b39084b11d86d7b98
Tyler Laceby <31893030+tylerbadger23@users.noreply.github.com>
2021-09-05 18:14:03 -0600

    ignore js

--- bin/newElectronApp.js
+++ bin/newElectronApp.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+//
 "use strict";
 var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
     if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
